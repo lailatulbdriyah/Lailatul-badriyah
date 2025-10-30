a0ccff82166b9146f3141e1662adea00b1bb9379
lailatulbdriyah <lailatulbdriyah0120@gmail.com>
2025-10-30 18:11:29 +1400

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// 1.Pilih tombol bedasarkan ID-nya
+const tombolSapa= dokumen.getElementByld('sapaButton')
+//2.Tambahkan 'event listener'untuk asli 'click'
+tombolsapa.addEventListener('click'function()
+//3.jalanlkan kode ini ketika tombol di-klik
+alert('Halo! Terima kasih sudah berkunjung!');
+});
