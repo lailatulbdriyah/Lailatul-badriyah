f174b4f870828fa22ded9417869026205d3ba3f9
lailatulbdriyah <lailatulbdriyah0120@gmail.com>
2025-10-30 17:56:15 +1400

    Create styll.s

---
+++ styll.s
@@ -0,0 +1,23 @@
+boody{
+      bacground-color:#FFFFE0: /*Warna kuning cerah */
+      font-family:Arial, sans-serif; /*jenis huruf yang bersih dan mudah di baca */
+      color:#191978; /* Warna teks utama(Biru Gelap) */
+{
+.kartu-profil
+    width:300px;
+    margin:50px auto; /*50px jarak atas/bawah,auto menengan kiri/kanan */
+    padding:20px
+    bacground-color:whitw;
+    border-radius: 15px; /*memebuat sudut menjadi melengkung */
+    box-shadow: 0 4px 8px rgba(0,0,0,0,1); /* Efek bayangan 
+
+
+
+  Kartu-profil img. {
+       width: 150px;
+       border adius: 50%; /trik untuk membuat gambar 
+
+ {
+ .kartu-profil {
+      /* ... (kode dari slide sebelumnya) ... */
+      texs-align: center; /*menengahkan semua texs dalam kartu */
